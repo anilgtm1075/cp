668797ec2dae0b05023ffc64866bd764876e2e6f
anilgtm1075 <108730178+anilgtm1075@users.noreply.github.com>
2023-03-21 12:51:27 +0530

    Update Sum of divisors.cpp
    count number of divisors and sum of divisors

--- Sum of divisors.cpp
+++ Sum of divisors.cpp
@@ -1,4 +1,5 @@
 #include<bits/stdc++.h>
+using namespace std;
 int sumOfDivisor(int n){
         int res = 1;
         for(int x = 2; x*x<=n; x++){
@@ -16,10 +17,24 @@ int sumOfDivisor(int n){
         if(n>1) res*=(n+1);
         return res;
 }
-using namespace std;
+int countFactors(int n){
+        //code here
+        int cnt = 1;
+        for(int x = 2; x*x<=n; x++){
+            int temp = 0;
+            while(n%x==0){
+                temp++;
+                n/=x;
+            }
+            cnt*=(temp+1);
+        }
+        if(n>1) cnt*=2;
+        return cnt;
+ }
 int main(){
   int n;
   cin>>n;
+  cout<<countFactors(n)<<endl;
   cout<<sumOfDivisor(n)<<endl;
   return 0;
 }
